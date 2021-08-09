9e288f9f829398562316ec7af1d6d07c4dad53bd
DipeshKrChaudhary <chaudharydipesh856@gmail.com>
2021-08-09 13:48:29 +0545

    Updates

---
+++ precache-manifest.fb396e998db6b2e2760b54932e07e629.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e9a0676fbd046ce4e3b5f96234732d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76920d4b4cfc2e301b70",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "810559dffed1bb213540",
+    "url": "/static/css/main.77f20ded.chunk.css"
+  },
+  {
+    "revision": "76920d4b4cfc2e301b70",
+    "url": "/static/js/2.c30a6aaa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c30a6aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810559dffed1bb213540",
+    "url": "/static/js/main.50537950.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
